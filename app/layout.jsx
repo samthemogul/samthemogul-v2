8b99ed0d2aa06d80419dfd6f4d983c44be652a25
Samuel Emeka <samuel.emeka@kibo.school>
2023-11-30 00:21:09 +0100

    First Commit

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import '@styles/globals.css';
+
+import Nav from '@/components/Nav';
+import Footer from '@/components/Footer';
+
+export const metadata  = {
+    title : "Sam Themogul",
+    description : "Portfolio"
+}
+
+const Rootlayout = ({ children }) => {
+    return (
+      <html lang='en'>
+          <body>
+              <div className="main">
+                  <div className="gradient" />
+              </div>
+              <main className="app">
+                  <Nav/>
+                  {children}
+                  <Footer/>
+              </main>
+          </body>
+      </html>
+    )
+  }
+  
+
+export default Rootlayout;
